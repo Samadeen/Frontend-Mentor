8a380e1d20174a581a80bddd2f318f62f0011c95
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-01-13 21:00:54 +0100

    feat: made button more dynamic

--- Junior/Single-page developer portfolio/src/layout/Button/Button.jsx
+++ Junior/Single-page developer portfolio/src/layout/Button/Button.jsx
@@ -1,7 +1,13 @@
 import styles from './Button.module.scss';
 
-const Button = ({ linkText, linkHref }) => {
-  return <div className={styles.btn}>{linkText}</div>;
+const Button = ({ linkText, linkHref, marginBottom }) => {
+  return (
+    <div
+      className={`${styles.btn} ${marginBottom ? styles.marginBottom : ''} `}
+    >
+      {linkText}
+    </div>
+  );
 };
 
 export default Button;
