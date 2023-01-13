6cf9ce4d12485bef4d8da165b3365d3c1ab6516a
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-01-13 21:00:28 +0100

    feat: added data for skills and project

---
+++ Junior/Single-page developer portfolio/src/data.js
@@ -0,0 +1,78 @@
+import imgOne from './assets/img1.jpg';
+import imgTwo from './assets/img2.png';
+import imgThree from './assets/img3.png';
+import imgFour from './assets/img4.png';
+import imgFive from './assets/img5.png';
+import imgSix from './assets/img6.png';
+
+export const skillData = [
+  {
+    title: 'HTML',
+    years: '4 Years Experience',
+  },
+  {
+    title: 'CSS',
+    years: '3 Years Experience',
+  },
+  {
+    title: 'Javascript',
+    years: '4 Years Experience',
+  },
+  {
+    title: 'Accessibility',
+    years: '4 Years Experience',
+  },
+  {
+    title: 'React',
+    years: '4 Years Experience',
+  },
+  {
+    title: 'Sass',
+    years: '3 Years Experience',
+  },
+];
+
+export const projectData = [
+  {
+    title: 'DESIGN PORTFOLIO',
+    img: imgOne,
+    html: 'HTML',
+    css: 'CSS',
+    javascript: '',
+  },
+  {
+    title: 'E-LEARNING LANDING PAGE',
+    img: imgTwo,
+    html: 'HTML',
+    css: 'CSS',
+    javascript: '',
+  },
+  {
+    title: 'TODO WEB APP',
+    img: imgThree,
+    html: 'HTML',
+    css: 'CSS',
+    javascript: 'JAVASCRIPT',
+  },
+  {
+    title: 'ENTERTAINMENT WEB APP',
+    img: imgFour,
+    html: 'HTML',
+    css: 'CSS',
+    javascript: 'JAVASCRIPT',
+  },
+  {
+    title: 'MEMORY GAME',
+    img: imgFive,
+    html: 'HTML',
+    css: 'CSS',
+    javascript: 'JAVASCRIPT',
+  },
+  {
+    title: 'ART GALLERY SHOWCASE',
+    img: imgSix,
+    html: 'HTML',
+    css: 'CSS',
+    javascript: 'JAVASCRIPT',
+  },
+];
