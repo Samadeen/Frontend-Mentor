5b689492c8117a532e6f600ee386b6e2d606102b
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-02-14 18:01:42 +0100

    Update index.jsx

--- Junior/Single-page developer portfolio/src/components/index.jsx
+++ Junior/Single-page developer portfolio/src/components/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Hero from './Hero/Hero';
+import Skills from './Skills/Skills';
+import Projects from './Projects/Projects';
+import Contact from './Contact/Contact';
 
 const Page = () => {
   return (
     <div>
       <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 };
