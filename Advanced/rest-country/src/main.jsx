f274c823ecfa24b154bcd8fbf13cc39a77027622
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-03-16 09:03:12 +0100

    Create main.jsx

---
+++ Advanced/rest-country/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.scss';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from './utils/theme-context';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
