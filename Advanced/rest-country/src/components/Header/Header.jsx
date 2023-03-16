0c2d698f84645cdf2ae0650ebb4aab3289e9ea7d
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-03-16 09:02:35 +0100

    Create Header.jsx

---
+++ Advanced/rest-country/src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import styles from './Header.module.scss';
+import moon from '../../assets/moon-icon.png';
+import { motion as m } from 'framer-motion';
+import { useContext } from 'react';
+import { ThemeContext } from '../../utils/theme-context';
+
+const Header = () => {
+  const { toggleTheme, theme } = useContext(ThemeContext);
+  return (
+    <m.header
+      id={theme}
+      animate={{ y: 0, opacity: 1 }}
+      initial={{ y: -100, opacity: 0 }}
+      className={styles.header_container}
+    >
+      <h1>Where in the world?</h1>
+      <div className={styles.mode}>
+        <img src={moon} alt='moon icon' onClick={toggleTheme} />
+        <p>Dark Mode</p>
+      </div>
+    </m.header>
+  );
+};
+
+export default Header;
