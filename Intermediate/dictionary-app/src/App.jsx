0e9b3b2b72172a4f0671b4b49cb71ef5b6390fb7
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-03-17 19:09:30 +0100

    Create App.jsx

---
+++ Intermediate/dictionary-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import Dictionary from '../test';
+import './App.css';
+import Header from './components/Header/Header';
+import SearchBar from './components/SearchBar/SearchBar';
+import Home from './pages/Home';
+import { ThemeContext } from './utils/theme-context';
+import { FontContext } from './utils/font-context';
+
+function App() {
+  const { font } = useContext(FontContext);
+  const { theme } = useContext(ThemeContext);
+
+  console.log(font);
+  return (
+    <div className={theme} id={font}>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
