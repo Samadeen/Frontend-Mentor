77f39bf21d342e34190783b53541a73b83413a76
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-06-06 12:12:01 +0100

    feat: added Header components

---
+++ Guru/audiophile-ecomm/src/layouts/Header/Header.tsx
@@ -0,0 +1,35 @@
+import logo from '../../assets/logo.svg';
+import carts from '../../assets/carts.svg';
+// import hamburger from '../../assets/hamburger.svg';
+import styles from './Header.module.scss';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.desktop_nav}>
+        <img src={logo} alt='logo' />
+        <nav>
+          <Link to='./'>Home</Link>
+          <Link to='./'>Headphones</Link>
+          <Link to='./'>Speakers</Link>
+          <Link to='./'>Earphones</Link>
+        </nav>
+        <img src={carts} alt='cart' />
+      </div>
+      {/* <div className={styles.mobile_nav}>
+        <img src={hamburger} alt="hamburger" />
+        <nav>
+          <Link to='./'>Home</Link>
+          <Link to='./'>Headphones</Link>
+          <Link to='./'>Speakers</Link>
+          <Link to='./'>Earphones</Link>
+        </nav>
+        <img src={logo} alt='logo' />
+        <img src={carts} alt='cart' />
+      </div> */}
+    </header>
+  );
+};
+
+export default Header;
