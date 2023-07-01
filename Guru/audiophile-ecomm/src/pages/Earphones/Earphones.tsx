72f400ca6303454440d7a261c018d207587516eb
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-07-01 12:12:01 +0100

    feat: added earphones page

---
+++ Guru/audiophile-ecomm/src/pages/Earphones/Earphones.tsx
@@ -0,0 +1,38 @@
+import { useContext } from 'react';
+import Layout from '../../layouts/Layout/Layout';
+import { EarphonesContext } from '../../api/earphones-api';
+import Card from '../../components/Card/Card';
+import Shop from '../../components/Shop/Shop';
+import Ad from '../../components/Ad/Ad';
+import styles from './Earphones.module.scss';
+
+const Earphones = () => {
+  const earphonesContext = useContext(EarphonesContext);
+
+  if (!earphonesContext) {
+    // Optional: Render a loading state or handle the null case
+    return <div>Loading...</div>;
+  }
+
+  const { earphones } = earphonesContext;
+
+  return (
+    <Layout>
+      <section className={styles.earphones_container}>
+        <h1>Earphones</h1>
+        <Card
+          description={earphones[0].description}
+          img={earphones[0].image.desktop}
+          reverse={false}
+          title={earphones[0].name}
+          newProduct={true}
+          id={earphones[0].id}
+        />
+        <Shop />
+        <Ad />
+      </section>
+    </Layout>
+  );
+};
+
+export default Earphones;
