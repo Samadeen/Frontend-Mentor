1fdb95afea83799c32fea5ac3af6290755893d42
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-07-16 12:12:01 +0100

    feat: added context provider

--- Guru/audiophile-ecomm/src/main.tsx
+++ Guru/audiophile-ecomm/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.scss';
+import { BrowserRouter } from 'react-router-dom';
+import { HeadphonesProvider } from './api/headphones-api.tsx';
+import { EarphonesProvider } from './api/earphones-api.tsx';
+import { SpeakersProvider } from './api/speakers-api.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <EarphonesProvider>
+        <SpeakersProvider>
+          <HeadphonesProvider>
+            <App />
+          </HeadphonesProvider>
+        </SpeakersProvider>
+      </EarphonesProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
