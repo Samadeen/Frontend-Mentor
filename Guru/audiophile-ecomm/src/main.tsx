51023ec2f7d61ccf517da58c17f9ab74585878b5
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-07-19 19:39:34 +0100

    Update main.tsx

--- Guru/audiophile-ecomm/src/main.tsx
+++ Guru/audiophile-ecomm/src/main.tsx
@@ -6,17 +6,23 @@ import { BrowserRouter } from 'react-router-dom';
 import { HeadphonesProvider } from './api/headphones-api.tsx';
 import { EarphonesProvider } from './api/earphones-api.tsx';
 import { SpeakersProvider } from './api/speakers-api.tsx';
+import { ShoppingCartProvider } from './contexts/cart-context.tsx';
+import { ProductProvider } from './api/product-api.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <EarphonesProvider>
-        <SpeakersProvider>
-          <HeadphonesProvider>
-            <App />
-          </HeadphonesProvider>
-        </SpeakersProvider>
-      </EarphonesProvider>
+      <ShoppingCartProvider>
+        <ProductProvider>
+          <EarphonesProvider>
+            <SpeakersProvider>
+              <HeadphonesProvider>
+                <App />
+              </HeadphonesProvider>
+            </SpeakersProvider>
+          </EarphonesProvider>
+        </ProductProvider>
+      </ShoppingCartProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
