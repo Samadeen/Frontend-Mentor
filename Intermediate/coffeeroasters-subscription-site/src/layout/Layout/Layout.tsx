5ac1826b3ea08fb089bbf099c16879c46863a941
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-08-18 12:12:01 +0100

    layout components

---
+++ Intermediate/coffeeroasters-subscription-site/src/layout/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import { ReactNode } from 'react';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
