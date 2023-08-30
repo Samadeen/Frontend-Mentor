001211b08bd35a3db93e09e14e89c86a893db701
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-08-30 21:39:45 +0100

    Update Hero.tsx

--- Intermediate/coffeeroasters-subscription-site/src/components/Hero/Hero.tsx
+++ Intermediate/coffeeroasters-subscription-site/src/components/Hero/Hero.tsx
@@ -5,11 +5,19 @@ import { motion as m } from 'framer-motion';
 const Hero = () => {
   return (
     <section className={styles.hero_container}>
-      <m.h1>
+      <m.h1
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.7, ease: 'easeInOut' }}
+      >
         Great coffee
         <br /> made simple.
       </m.h1>
-      <m.p>
+      <m.p
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+      >
         Start your mornings with the world’s best coffees. Try our expertly
         curated artisan coffees from our best roasters delivered directly to
         your door, at your schedule.
